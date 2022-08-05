a65bf63d3bd968b0fa4616cd9ca20a04ea3f0ca4
mklapshin <golub.97@mail.ru>
2022-08-05 18:17:09 +0300

    homework5

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export {Author} from './author.js'
+export {BookCollection} from './book-collection.js'
+export {Book} from './book.js'
+export {Collection} from './collection.js'
+export {Notepad} from './notepad.js'
+export {Product} from './product.js'
+export {Ratingable} from './ratingable.js'
